25c25fe388cd5c50ec4d6afa6c689da9009e0259
Anand Kumar <anand.kumar2@comviva.com>
2022-04-06 16:00:52 +0530

    Summation module framework

---
+++ Sum_sdk/Sum_sdk/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Sum_sdk
+//
+//  Created by Anand on 06/04/22.
+//
+
+import Foundation
+
+public class Addition {
+    private init() {}
+    public static func addition(a : Int , b : Int) -> Int {
+        return (a + b)
+    }
+}
---
+++ Sum_sdk/Sum_sdk/Sum_sdk.h
@@ -0,0 +1,18 @@
+//
+//  Sum_sdk.h
+//  Sum_sdk
+//
+//  Created by Anand on 06/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sum_sdk.
+FOUNDATION_EXPORT double Sum_sdkVersionNumber;
+
+//! Project version string for Sum_sdk.
+FOUNDATION_EXPORT const unsigned char Sum_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sum_sdk/PublicHeader.h>
+
+
